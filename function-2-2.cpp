39dc7b11e370713e3947858bd800c8385903b959
EvPev <evgaylard@gmail.com>
2024-07-29 21:25:21 +0930

    Update function-2-2.cpp

--- function-2-2.cpp
+++ function-2-2.cpp
@@ -8,9 +8,10 @@ int binary_to_int(int binary_digest[], int number_of_digits) {
     int sum = 0;
 
     for (int i = 0; i < number_of_digits; i++) {
-        if (binary_digest[i] == 1) {
+        if (binary_digest[number_of_digits - 1 - i] == 1) {
             double di = i;
             sum = sum + pow(2,di);
+            cout << sum<<endl;
         }
         
     }
