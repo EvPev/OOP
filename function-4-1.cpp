eead74d07341894501b69933c751aa6e79b4df8e
EvPev <evgaylard@gmail.com>
2024-08-14 13:34:13 +0930

    Update function-4-1.cpp

--- function-4-1.cpp
+++ function-4-1.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-int* readNumbers()
+int* readNumbers(int length)
 {
-    int* array = new int[10];
-    cout << "Enter 10 numbers" << endl;
+    int* array = new int[length];
 
-    for (int i = 0; i < 10; i++)
+    for (int i = 0; i < length; i++)
     {
-        cout << "Enter number [" << i+1 << "/10]: ";
+        cout << "Enter number [" << i+1 << "/" << length << "]: ";
         cin >> array[i];
     }
 
@@ -21,54 +21,47 @@ int secondSmallestSum(int *numbers,int length)
     if (length <= 1) {return 0;}
     int subArrays = length * (length + 1)/2;
     int *sums = new int[subArrays];
+    int counter = 0;
 
     int tempSum = 0;
     for (int k = 0; k < length; k++)
     {
-        for (int i = 0 + k; i < length; i++)
+        for (int i = 0; i < length - k; i++)
         {
             tempSum = 0;   
             for (int j = 0 + k; j < (length - i); j++)
             {
                 tempSum = tempSum + numbers[j];
             }
-            sums[i] = tempSum;
+            sums[counter] = tempSum;
+            counter++;
         }
     }
 
     for (int i = 0; i < subArrays; i++)
     {
-        cout << sums[i] << " " << endl;
+        cout << sums[i] << " ";
     }
+    cout << endl;
 
 
 
 
-    int secondSum = sums[0];
-    int smallestSum = sums[0];
+    int secondSmallest = INT_MAX;
+    int smallestSum = INT_MAX;
 
     for (int i = 0; i < subArrays; i++)
     {
+        if (secondSmallest > smallestSum && secondSmallest > sums[i]) {
+            secondSmallest = sums[i];
+        }
         if (smallestSum > sums[i])
         {
-            secondSum = smallestSum;
             smallestSum = sums[i];
-        }   
+        }  
     }
 
-    if (smallestSum == secondSum) 
-    {
-        smallestSum = sums[1];
-        for (int i = 0; i < subArrays; i++)
-        {
-            if (smallestSum > sums[i])
-            {
-                secondSum = smallestSum;
-                smallestSum = sums[i];
-            }   
-        } 
-    }
 
     delete[] sums;
-    return secondSum;
+    return secondSmallest;
 }
