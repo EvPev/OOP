9aeaf4781dc4a2dcd8922977e433926276d8097d
Evpev <evgaylard@gmail.com>
2024-08-27 11:52:08 +0930

    update

--- ParkingLot.cpp
+++ ParkingLot.cpp
@@ -8,6 +8,10 @@ ParkingLot::ParkingLot(int capacity)
 {
     this->capacity = capacity;
     vehicles = new Vehicle*[capacity];
+    for (int i = 0; i < capacity; i++)
+    {
+        vehicles[i] = new Vehicle();
+    }
 }
 
 int ParkingLot::getCount() 
@@ -17,8 +21,36 @@ int ParkingLot::getCount()
     {
         if (vehicles[i]->get_real() == 1) {count++;}
     }
-    
+    return count;
 }
 
-void ParkingLot::parkVehicle(Vehicle* parkingVehicle) {}
-void ParkingLot::unparkVehicle(int ID) {}
+void ParkingLot::parkVehicle(Vehicle* parkingVehicle) 
+{
+    bool spaceFound = false;
+        for (int i = 0; i < capacity; i++)
+    {
+        if (vehicles[i]->get_real() == 0) 
+        {
+            spaceFound = 1;
+            delete vehicles[i];
+            vehicles[i] = parkingVehicle;
+            break;
+        }
+    }
+    if (spaceFound == 0) {cout << "The lot is full" << endl;}
+}
+
+void ParkingLot::unparkVehicle(int ID) 
+{
+    bool unparkedTrue = 0;
+    for (int i = 0; i < capacity; i++)
+    {
+        if (vehicles[i]->get_real() == 1 && vehicles[i]->get_ID() == ID)
+        {
+            delete vehicles[i];
+            vehicles[i] = new Vehicle();
+            unparkedTrue = 1;
+        }
+    }
+    if (unparkedTrue == 0) {cout << "Vehicle not in the lot" << endl;}
+}
--- main-1-2.cpp
+++ main-1-2.cpp
@@ -3,10 +3,45 @@
 #include "Bus.h"
 #include "Car.h"
 #include "Motorbike.h"
+#include "ParkingLot.h"
 
 using namespace std;
 
 int main() 
 {
+    int capacity = 3;
+    ParkingLot p_lot(capacity);
+    string v_type;
+    int ID;
+    for (int i = 0; i < capacity; i++)
+    {
+        cout << "Vehicle type: ";
+        cin >> v_type;
+        cout << "Vehicle ID: ";
+        cin >> ID;
+        
+        Vehicle* tempPark;
+
+        if (v_type == "car") {tempPark = new Car(ID);}
+        if (v_type == "bus") {tempPark = new Bus(ID);}
+        if (v_type == "motorbike") {tempPark = new Motorbike(ID);}
+        p_lot.parkVehicle(tempPark);
+    }
+
+    
+    cout << "\n[park]\n";
+    Vehicle* carr = new Car(10);
+    p_lot.parkVehicle(carr);
+
+    
+
+    cout << "\n[Unpark]\n Vehicle ID: ";
+    cin >> ID;
+    p_lot.unparkVehicle(ID);
+
+    cout << "\n[park]\n";
+    p_lot.parkVehicle(carr);
+
+
     
 }
