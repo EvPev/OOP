d7a96614436c78b05e983a4a2348811338aa25e8
Evpev <evgaylard@gmail.com>
2024-08-28 12:56:02 +0930

    update

--- Car.h
+++ Car.h
@@ -1,11 +1,13 @@
 #ifndef CAR_H
 #define CAR_H
 #include <iostream>
+#include "Vehicle.h"
 
 using namespace std;
 
 
-class Car: public Vehicle {
+class Car: public Vehicle 
+{
     public:
         Car();
         Car(int ID);
