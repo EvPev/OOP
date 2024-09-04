acda23388f12aa946be15c418ff3a64b1bfa0fdb
Evpev <evgaylard@gmail.com>
2024-09-04 14:40:37 +0930

    m

--- Helicopter.cpp
+++ Helicopter.cpp
@@ -13,8 +13,8 @@ void Helicopter::set_name(string name) {this->name = name;}
 
 void Helicopter::fly(int headwind, int minutes){
     double tempFuel = fuel;
-    if (headwind <= 40) {tempFuel -= 0.2 * minutes;}
-    else if (headwind >= 40) {tempFuel -= 0.4 * minutes;}
+    if (headwind < 40) {tempFuel = tempFuel - (0.2 * minutes);}
+    else if (headwind >= 40) {tempFuel = tempFuel - (0.4 * minutes);}
 
     if (weight > 5670) {tempFuel -= ((0.01 * (weight - 5670)) * minutes);}
 
