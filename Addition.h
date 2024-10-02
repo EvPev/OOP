155c0d638b7b7de9302d1d454f2a698ab7a6469e
Evpev <evgaylard@gmail.com>
2024-10-02 11:13:46 +0930

    m

---
+++ Addition.h
@@ -0,0 +1,14 @@
+#ifndef ADDITION_H
+#define ADDITION_H
+#include <iostream>
+
+using namespace std;
+
+class Addition {
+public:
+    int add(int lhs, int rhs) {
+        return lhs + rhs;
+    }
+};
+
+#endif
