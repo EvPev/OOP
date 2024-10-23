fcbcdc1ab26d4a84e92f65149c6d96670a8726ed
Evpev <evgaylard@gmail.com>
2024-10-23 13:00:13 +1030

    static

--- main-1.cpp
+++ main-1.cpp
@@ -5,8 +5,8 @@
 #define print(x) std::cout << x << std::endl;
 
 int main() {
-    std::tuple pos1= Utils::generateRandomPos(10,10);
-    std::tuple pos2= Utils::generateRandomPos(10,10);
+    std::tuple<int,int> pos1= Utils::generateRandomPos(10,10);
+    std::tuple<int,int> pos2= Utils::generateRandomPos(10,10);
     GameEntity E1(std::get<0>(pos1), std::get<1>(pos1), GameEntity::NoneType);
     GameEntity E2(std::get<0>(pos2), std::get<1>(pos2), GameEntity::NoneType);
 
